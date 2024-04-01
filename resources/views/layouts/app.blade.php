7f8c2ee5a0b29c81a3de7da0190aa41a09f277f5
Arif Rahman <arivtech7@gmail.com>
2024-04-01 10:44:55 +0700

    login using google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="{{ asset('resources/app.css') }}"> --}}
-    {{-- @vite('resources/js/app.js') --}}
+
 
     <title>Laracamp by BuildWith Angga</title>
 </head>
@@ -22,6 +21,7 @@
 <body>
 
     @include('components.navbar')
+    {{-- @include('layouts.navigation') --}}
 
     @yield('content')
 
